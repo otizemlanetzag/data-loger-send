91154966e6760a456803b1561bed79e5fb97f3c3
otizemlanetzag <urielhetzroni@gmail.com>
2026-05-02 11:45:47 +0300

    Update main.ts

--- main.ts
+++ main.ts
@@ -2,45 +2,41 @@
 namespace SmartLogger {
 
     /**
-     * רושם את כל העמודות לאחסון הפנימי ושולח אותן כהודעה אחת ברדיו
+     * רשום נתונים לאוגר הפנימי ושולח אותם אוטומטית ברדיו
      */
     //% block="רשום נתונים $data"
     //% data.shadow="lists_create_with"
     //% data.defl="smart_log_create_cv"
     export function logAndRadio(data: any[]): void {
-        // 1. רישום לאוגר הפנימי (עבור קובץ ה-HTM)
-        const dl = (datalogger as any);
-        dl.logData(data);
+        // @ts-ignore
+        datalogger.logData(data);
 
-        // 2. בניית מחרוזת אחת מכל העמודות ושליחה ברדיו
         let message = "";
         for (let item of data) {
-            let column = (item as any).column;
-            let value = (item as any).value;
-            message += column + ":" + value + " ";
+            // @ts-ignore
+            message += item.column + ":" + item.value + " ";
         }
-
         if (message.length > 0) {
             radio.sendString(message.trim());
         }
     }
 
     /**
-     * יוצר זוג של עמודה וערך (הבלוק שמתחבר לתוך הרשימה)
+     * יוצר זוג של עמודה וערך
      */
     //% block="עמודה $column ערך $value"
     //% blockId=smart_log_create_cv
-    //% blockHidden=false
-    //% value.shadow=math_number
     export function createCV(column: string, value: any): any {
-        return (datalogger as any).createCV(column, value);
+        // @ts-ignore
+        return datalogger.createCV(column, value);
     }
 
     /**
-     * מחיקת כל הנתונים מהזיכרון
+     * מחיקה
      */
     //% block="מחיקת כל הנתונים"
     export function clearAll(): void {
-        (datalogger as any).deleteLog();
+        // @ts-ignore
+        datalogger.deleteLog();
     }
 }
